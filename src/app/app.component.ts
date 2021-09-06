d42a253c5334310bc82e34b3604dbb74f247a22d
renny-kunjumon <renny.kunjumon@rws.com>
2021-09-07 01:46:16 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component, VERSION } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  users: any;
+
+  constructor(private api: ApiService) {}
+
+  ngOnInit() {
+    this.api.get('users?page=1').subscribe(res => {
+      this.users = res;
+      console.log('data response', this.users);
+    });
+  }
 }
